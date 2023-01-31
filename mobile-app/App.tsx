982201a93679f983610f7c030c796c4c4953eba2
Arnaud Renaud <arnaud.renaud@gmail.com>
2023-01-31 14:33:53 +0100

    Set up push notifications
    Display notification content on screen when received.

--- mobile-app/App.tsx
+++ mobile-app/App.tsx
@@ -1,11 +1,16 @@
 import { StatusBar } from "expo-status-bar";
 import Constants from "expo-constants";
+
+import * as Notifications from "expo-notifications";
 import { SafeAreaProvider } from "react-native-safe-area-context";
 import { ApolloClient, ApolloProvider, InMemoryCache } from "@apollo/client";
 
 import useCachedResources from "./hooks/useCachedResources";
 import useColorScheme from "./hooks/useColorScheme";
 import Navigation from "./navigation";
+import { useState, useRef, useEffect } from "react";
+import { registerForPushNotificationsAsync } from "./push-notifications";
+import { View, Text } from "./components/Themed";
 
 const GRAPHQL_API_URL = Constants?.expoConfig?.extra?.GRAPHQL_API_URL;
 
@@ -14,9 +19,45 @@ const client = new ApolloClient({
   cache: new InMemoryCache(),
 });
 
+Notifications.setNotificationHandler({
+  handleNotification: async () => ({
+    shouldShowAlert: true,
+    shouldPlaySound: false,
+    shouldSetBadge: false,
+  }),
+});
+
 export default function App() {
   const isLoadingComplete = useCachedResources();
   const colorScheme = useColorScheme();
+
+  const [expoPushToken, setExpoPushToken] = useState("");
+  const [notification, setNotification] = useState(false);
+  const notificationListener = useRef();
+  const responseListener = useRef();
+
+  useEffect(() => {
+    registerForPushNotificationsAsync().then((token) =>
+      setExpoPushToken(token)
+    );
+
+    notificationListener.current =
+      Notifications.addNotificationReceivedListener((notification) => {
+        setNotification(notification);
+      });
+
+    responseListener.current =
+      Notifications.addNotificationResponseReceivedListener((response) => {
+        console.log(response);
+      });
+
+    return () => {
+      Notifications.removeNotificationSubscription(
+        notificationListener.current
+      );
+      Notifications.removeNotificationSubscription(responseListener.current);
+    };
+  }, []);
 
   if (!isLoadingComplete) {
     return null;
@@ -24,6 +65,28 @@ export default function App() {
     return (
       <SafeAreaProvider>
         <ApolloProvider client={client}>
+          <View
+            style={{
+              flex: 1,
+              alignItems: "center",
+              justifyContent: "space-around",
+            }}
+          >
+            <Text>Your expo push token: {expoPushToken}</Text>
+            <View style={{ alignItems: "center", justifyContent: "center" }}>
+              <Text>
+                Title: {notification && notification.request.content.title}{" "}
+              </Text>
+              <Text>
+                Body: {notification && notification.request.content.body}
+              </Text>
+              <Text>
+                Data:{" "}
+                {notification &&
+                  JSON.stringify(notification.request.content.data)}
+              </Text>
+            </View>
+          </View>
           <Navigation colorScheme={colorScheme} />
           <StatusBar />
         </ApolloProvider>
